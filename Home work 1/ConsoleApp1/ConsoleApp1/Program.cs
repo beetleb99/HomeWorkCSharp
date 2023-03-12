6a222a1f6afeae8d24627f1eb3788bb6583e990d
Bekzod <bekzod.abdullaev.1999@gmail.com>
2023-03-12 14:55:19 +0700

    Home Work 1

---
+++ Home work 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace homeWork;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Как тебя зовут?");
+        string userName = Console.ReadLine();
+        Console.WriteLine($"Привет,{userName}");
+    }
+}
+
